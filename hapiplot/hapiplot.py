c2e3cfb4e4c74e13118a3ba0f12a26c24423470d
Bob Weigel <rweigel@gmu.edu>
2021-06-16 13:27:20 -0400

    Last 0.2.0 commit

--- hapiplot/hapiplot.py
+++ hapiplot/hapiplot.py
@@ -17,7 +17,7 @@ from hapiplot.plot.util import setopts
 def hapiplot(*args, **kwargs):
     """Plot response from HAPI server.
 
-    Version: 0.0.1b6
+    Version: 0.2.0
     
     Demos
     -----
