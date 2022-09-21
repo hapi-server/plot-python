d64b5711cf722072d8d5cc2e7b62082943f5dec0
Bob Weigel <rweigel@gmu.edu>
2022-09-21 16:36:11 -0400

    version

--- hapiplot/hapiplot.py
+++ hapiplot/hapiplot.py
@@ -17,7 +17,7 @@ from hapiplot.plot.util import setopts
 def hapiplot(*args, **kwargs):
     """Plot response from HAPI server.
 
-    Version: 0.2.1
+    Version: 0.2.2b0
     
     Demos
     -----
